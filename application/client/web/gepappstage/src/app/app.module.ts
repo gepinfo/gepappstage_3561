8befb7f4aa14ff01adf56d6793f17e2f3b44fccb
gepinfo <gepinfo>
2022-11-15 07:49:14 +0000

    updated generated code

--- application/client/web/gepappstage/src/app/app.module.ts
+++ application/client/web/gepappstage/src/app/app.module.ts
@@ -1,13 +1,16 @@
-import { SharedService } from '../shared/shared.service';
-import { TemplateModule } from './template/template.module';
-import { FooterModule } from './footer/footer.module';
-import { HeaderModule } from './header/header.module';
-import { TranslatorModule } from './translator/translator.module';
-import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppComponent } from './app.component';
-import { BrowserModule } from '@angular/platform-browser';
+import { SefscreenModule } from './sefscreen/sefscreen.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { TranslatorModule } from './translator/translator.module';
+import { HeaderModule } from './header/header.module';
+import { FooterModule } from './footer/footer.module';
+import { TemplateModule } from './template/template.module';
+import { SharedService } from '../shared/shared.service';
 import { LoginModule } from './login/login.module';
 import { SignupModule } from './signup/signup.module';
 import { HomeModule } from './home/home.module';
@@ -17,22 +20,28 @@ import { ManageusersModule } from './manageusers/manageusers.module';
 import { ManagecontrolModule } from './managecontrol/managecontrol.module';
 import { UserModule } from './user/user.module';
 import { VaultadminModule } from './vaultadmin/vaultadmin.module';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SefscreenModule } from './sefscreen/sefscreen.module';
 
 @NgModule({
   declarations: [
-      AppComponent
+        AppComponent
 ],
   imports: [
-      BrowserModule,
-BrowserAnimationsModule,
-AppRoutingModule,
-TranslatorModule,
-HeaderModule,
-FooterModule,
+  SefscreenModule,
+FormsModule,
+HttpClientModule,
+VaultadminModule,
+ManagecontrolModule,
+ManageusersModule,
+ManagerolesModule,
+AuthorizationModule,
 TemplateModule,
+FooterModule,
+HeaderModule,
+TranslatorModule,
+AppRoutingModule,
+BrowserAnimationsModule,
+      BrowserModule,
 LoginModule,
 SignupModule,
 HomeModule,
@@ -42,12 +51,10 @@ ManageusersModule,
 ManagecontrolModule,
 UserModule,
 VaultadminModule,
-HttpClientModule,
-FormsModule,
 SefscreenModule
 ],
   providers: [
-  	SharedService
+    	SharedService
 ],
   bootstrap: [
   ]
